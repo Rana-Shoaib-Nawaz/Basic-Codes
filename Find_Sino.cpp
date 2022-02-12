72941b929f0a7a6f3856ade7c1b55bdda27dd866
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 02:40:34 +0500

    Find Sino

---
+++ Find_Sino.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+	double angle1,y;
+	cout<<"Enter the angle1 :";
+	cin>>angle1;
+	y=1/sin(angle1);
+	cout<<"y="<<y;
+	return 0;
+}
