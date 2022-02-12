0ff61454ac4216b067432e003e4f5f68a82020f6
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 01:31:06 +0500

    Find Area Of area

---
+++ Find_Area_Of_Pizza.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	double diameter,slice,area,pi;
+	cout<<"Enter the diameter :";
+	cin>>diameter;
+	pi=3.14;
+	area=pi*(diameter/2)*(diameter/2);
+	slice=area/14.125;
+	cout<<"Area of pizza "<<area<<endl<<"Number of slice "<<slice;
+	return 0;
+}
