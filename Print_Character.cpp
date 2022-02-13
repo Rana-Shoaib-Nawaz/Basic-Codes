9cf8648ee844c58bd166a950dee1f3365b85fd93
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 14:55:38 +0500

    Print The Character

---
+++ Print_Character.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	char first,middle,last;
+	first='a';
+	middle='b';
+	last='c';
+	cout<<"First="<<first<<endl<<"Middle="<<middle<<endl<<"Last="<<last;
+	return 0;
+}
