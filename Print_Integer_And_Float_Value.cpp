1680b0dcc4ab94bb9e9a86a81da904820b1aefb8
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 15:00:35 +0500

    Print Integer And Float Value

---
+++ Print_Integer_And_Float_Value.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int age;
+	float weight;
+	age=20;
+	weight=60.1;
+	cout<<"My age is "<<age<<" and my weight is "<<weight<<" pounds";
+	return 0;
+}
