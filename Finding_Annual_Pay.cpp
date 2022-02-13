cb96073f45fa60ae1a8a93e16ee89a62c4f9a2e8
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 14:10:36 +0500

    Find The Annual Pay

---
+++ Finding_Annual_Pay.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int payamount,payperiod,annualpay;
+	payamount=5700;
+	payperiod=26;
+	annualpay=payamount*payperiod;
+	cout<<"total annual pay="<<annualpay;
+	return 0;
+}
