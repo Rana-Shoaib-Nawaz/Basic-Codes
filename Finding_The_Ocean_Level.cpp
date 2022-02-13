49c2fe48dece7f9c61271e61dedd2e22512ac7b7
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 14:42:44 +0500

    Finding The Ocean Level

---
+++ Finding_The_Ocean_Level.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int year;
+	float millimeters,oceanlevel1,oceanlevel2,oceanlevel3;
+	millimeters=1.2;
+	oceanlevel1=millimeters*5;
+	oceanlevel2=millimeters*7;
+	oceanlevel3=millimeters*10;
+	cout<<"will be in 5 year="<<oceanlevel1<<" will be in 7 year="<<oceanlevel2<<" will be in 10 year="<<oceanlevel3;
+	return 0;
+}
