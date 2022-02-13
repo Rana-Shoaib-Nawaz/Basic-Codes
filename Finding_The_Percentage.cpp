44b5c980c5227e5e2d7c8a0464c3fd14ed766f11
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 14:46:47 +0500

    Finding The Percentage

---
+++ Finding_The_Percentage.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int Rs,division;
+	float percent,sale,totalsale;
+	Rs=680000000;
+	division=100;
+	percent=62;
+	sale=Rs*percent/division;
+	cout<<"total sale="<<sale;
+	return 0;
+}
