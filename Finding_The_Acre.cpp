556261c74a9660a2d47e7d6aa9d256757b007603
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 14:32:21 +0500

    Finding The Acre

---
+++ Finding_The_Acre.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int squarefeet;
+    float acre,oneacre;
+    oneacre=43560;
+    squarefeet=389767;
+    acre=squarefeet/oneacre;
+    cout<<"Acre="<<acre;
+    return 0;
+}
