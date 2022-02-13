c2394ecab0bfc0c223e0c6747ec6674e44529fcd
Rana-Shoaib-Nawaz <ranashoaib6162@gmail.com>
2022-02-13 18:38:11 +0500

    Find The Distance

---
+++ Find_Distance.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+ float a,z;   
+int t,v,x;
+a=-9.81;
+t=10;
+v=0;   //initial velocity
+x=0;   //initial position
+z=0.5*a*t*t+v*t+x;
+cout<<z<<"m";	
+}
+
